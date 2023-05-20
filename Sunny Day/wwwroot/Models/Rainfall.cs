f1144500d328da18c1103c58de1dd70f055bd94c
Sarah Quinn <ssa.quinn@gmail.com>
2023-05-20 14:35:41 +1200

    git commit -m "startup"

---
+++ Sunny Day/wwwroot/Models/Rainfall.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Sunny_Day.wwwroot.Models
+{
+    public class Rainfall
+    {
+        public double[] rainfall { get; set; }
+        public DateTime[] timeOfDay { get; set; }
+    }
+}
---
+++ Sunny Day/wwwroot/Services/JsonFileRainfallService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Sunny_Day.wwwroot.Services
+{
+    public class JsonFileRainfallService
+    {
+        public JsonFileRainfallService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "mockdata.json"); }
+        }
+
+        public IEnumerable<Rainfall> GetRainfall()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Rainfall[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
