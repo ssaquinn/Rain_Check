6665f711af9bb14ba5991122c997d534514b5d5e
Finn Brightwell <gamebyfizz@gmail.com>
2023-05-20 15:33:20 +1200

    Restored structure to .net 6

--- Sunny Day/Program.cs
+++ Sunny Day/Program.cs
@@ -1,13 +1,29 @@
-using Sunny_Day;
-
+using Sunny_Day.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-var startup = new Startup(builder.Configuration); // My custom startup class.
 
-startup.ConfigureServices(builder.Services); // Add services to the container.
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddTransient<JsonFileRainfallService>();
 
 var app = builder.Build();
 
-startup.Configure(app, app.Environment); // Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
